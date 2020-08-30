7fd33f50419e7005361d2750932b7f47a07eb817
awkshwayrd <mathur.aksh@gmail.com>
2020-08-31 02:01:40 +0530

    kotlin Coffee Machine Stage 3/6 Enough coffee for everyone

---
+++ kotlin/Coffee Machine/Problems/Print two numbers on the same line/src/Main.kt
@@ -0,0 +1,10 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    // put your code here
+    val one = scanner.nextInt()
+    val two = scanner.nextInt()
+    println("$one $two")
+
+}
