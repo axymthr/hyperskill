cf95e806b700130329692ed85e9555a439c3dcb5
awkshwayrd <mathur.aksh@gmail.com>
2020-08-31 22:28:41 +0530

    kotlin Coffee Machine Stage 3/6 Enough coffee for everyone done

--- kotlin/Coffee Machine/Coffee Machine/task/src/machine/Main.kt
+++ kotlin/Coffee Machine/Coffee Machine/task/src/machine/Main.kt
@@ -4,13 +4,34 @@ import java.util.*
 
 fun main() {
     val scanner = Scanner(System.`in`)
+    println("Write how many ml of water the coffee machine has:")
+    val actlWater = scanner.nextInt() //1550
+    println("Write how many ml of milk the coffee machine has:")
+    val actlMilk = scanner.nextInt() //299
+    println("Write how many grams of coffee beans the coffee machine has:")
+    val actlCoffee = scanner.nextInt() //300
     println("Write how many cups of coffee you will need:")
-    val cups = scanner.nextInt()
-    println("For $cups cups of coffee you will need:")
-    val water = 200*cups
-    println("$water ml of water")
-    val milk = 50*cups
-    println("$milk ml of milk")
-    val beans = 15*cups
-    println("$beans g of coffee beans")
+    val cups = scanner.nextInt() //3
+    val water = 200
+    val milk = 50
+    val beans = 15
+    val waterUnits = actlWater / water //300 / 200 = 1
+    val milkUnits = actlMilk / milk //65 / 50 = 1
+    val coffeeUnits = actlCoffee / beans // 111 / 15 = 7
+    var actual = if (waterUnits > milkUnits) milkUnits else waterUnits //
+    actual = if (coffeeUnits > actual) actual else coffeeUnits
+
+    when {
+        cups == actual -> {
+            println("Yes, I can make that amount of coffee")
+        }
+        cups > actual -> {
+            println("No, I can make only $actual cups of coffee")
+        }
+        else -> {
+            println("Yes, I can make that amount of coffee (and even ${actual - cups} more than that)")
+        }
+    }
 }
+
+
