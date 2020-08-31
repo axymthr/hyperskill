161ebefad65d5689260d1dc2a8c0bf9be209e241
awkshwayrd <mathur.aksh@gmail.com>
2020-08-31 22:46:57 +0530

    kotlin Coffee Machine when Direction

---
+++ kotlin/Coffee Machine/Problems/Direction/src/Main.kt
@@ -0,0 +1,16 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+
+    // write your code here
+    val dir = scanner.nextInt()
+    println(when (dir) {
+        0 -> "do not move"
+        1 -> "move up"
+        2 -> "move down"
+        3 -> "move left"
+        4 -> "move right"
+        else -> "error!"
+    })
+}
