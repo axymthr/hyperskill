8f67e4c76ad122e92d46e958af2df6191545e9a9
awkshwayrd <mathur.aksh@gmail.com>
2020-08-31 22:58:43 +0530

    kotlin Coffee Machine when Floor-space of the room

---
+++ kotlin/Coffee Machine/Problems/Floor-space of the room/src/Main.kt
@@ -0,0 +1,29 @@
+import java.util.*
+import kotlin.math.sqrt
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+
+    // write your code here
+    when (scanner.next()) {
+        "triangle" -> {
+            val a = scanner.nextDouble()
+            val b = scanner.nextDouble()
+            val c = scanner.nextDouble()
+            val p = (a+b+c)/2
+            val area = sqrt(p*(p-a)*(p-b)*(p-c))
+            println(area)
+        }
+        "rectangle" -> {
+            val a = scanner.nextDouble()
+            val b = scanner.nextDouble()
+
+            println(a*b)
+        }
+        "circle" -> {
+            val r = scanner.nextDouble()
+            val area = 3.14*r*r
+            println(area)
+        }
+    }
+}
