2bdb1685a375aeac7cb6283ca71789148d47a91e
awkshwayrd <mathur.aksh@gmail.com>
2020-09-02 21:04:10 +0530

    kotlin Coffee Machine Stage 6/6: Brush up your code

--- kotlin/Coffee Machine/Coffee Machine/task/src/machine/Main.kt
+++ kotlin/Coffee Machine/Coffee Machine/task/src/machine/Main.kt
@@ -3,15 +3,100 @@ package machine
 import java.util.*
 
 fun main() {
-
     val scanner = Scanner(System.`in`)
+    val coffeeMachine = CoffeeMachine()
+    do {
+        println(coffeeMachine.prompt())
+        val action = scanner.next()
+        coffeeMachine.action(action)
+    } while (action != "exit")
+}
+
+class CoffeeMachine {
     var money = 550
     var water = 400
     var milk = 540
     var beans = 120
     var cups = 9
+    var menuState = CoffeeMachineMenuState.MAIN
 
-    fun makeCoffee(useWater: Int, useMilk: Int, useBeans: Int, addMoney: Int) {
+    fun prompt(): String {
+        return when (menuState) {
+            CoffeeMachineMenuState.BUY -> {
+                println("Prompt BUY")
+                "What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:"
+            }
+            CoffeeMachineMenuState.FILL_WATER -> "Write how many ml of water do you want to add:"
+            CoffeeMachineMenuState.FILL_MILK -> "Write how many ml of milk do you want to add:"
+            CoffeeMachineMenuState.FILL_BEANS -> "Write how many grams of coffee beans do you want to add:"
+            CoffeeMachineMenuState.FILL_CUPS -> "Write how many disposable cups of coffee do you want to add:"
+            else -> {
+                menuState == CoffeeMachineMenuState.FILL_WATER
+                "Write action (buy, fill, take, remaining, exit): "
+            }
+        }
+    }
+
+    fun action(action: String) {
+        when {
+            menuState == CoffeeMachineMenuState.MAIN && action == "buy" -> {
+                menuState = CoffeeMachineMenuState.BUY
+            }
+
+            menuState == CoffeeMachineMenuState.MAIN && action == "fill" -> {
+                menuState = CoffeeMachineMenuState.FILL_WATER
+            }
+
+            menuState == CoffeeMachineMenuState.MAIN && action == "take" -> emptyMoney()
+
+            menuState == CoffeeMachineMenuState.MAIN && action == "remaining" -> printState()
+
+            menuState == CoffeeMachineMenuState.FILL_WATER -> {
+                water += action.toInt()
+                menuState = CoffeeMachineMenuState.FILL_MILK
+            }
+
+            menuState == CoffeeMachineMenuState.FILL_MILK -> {
+                milk += action.toInt()
+                menuState = CoffeeMachineMenuState.FILL_BEANS
+            }
+
+            menuState == CoffeeMachineMenuState.FILL_BEANS -> {
+                beans += action.toInt()
+                menuState = CoffeeMachineMenuState.FILL_CUPS
+            }
+
+            menuState == CoffeeMachineMenuState.FILL_CUPS -> {
+                cups += action.toInt()
+                menuState = CoffeeMachineMenuState.MAIN
+            }
+
+            menuState == CoffeeMachineMenuState.BUY -> {
+                when (action) {
+                    "1" -> makeCoffee(250, 0, 16, 4)
+                    "2" -> makeCoffee(350, 75, 20, 7)
+                    "3" -> makeCoffee(200, 100, 12, 6)
+                    else -> {
+                    }
+                }
+                menuState = CoffeeMachineMenuState.MAIN
+            }
+
+            else -> {
+            }
+        }
+    }
+
+    private fun printState() {
+        println("The coffee machine has:")
+        println("$water of water")
+        println("$milk of milk")
+        println("$beans of coffee beans")
+        println("$cups of disposable cups")
+        println("$money of money")
+    }
+
+    private fun makeCoffee(useWater: Int, useMilk: Int, useBeans: Int, addMoney: Int) {
         if (!enough(water, useWater, "water") ||
                 !enough(milk, useMilk, "milk") ||
                 !enough(beans, useBeans, "coffee beans") ||
@@ -25,53 +110,19 @@ fun main() {
         money += addMoney
     }
 
-    fun fillMachine() {
-        println("Write how many ml of water do you want to add:")
-        water += scanner.nextInt()
-        println("Write how many ml of milk do you want to add:")
-        milk += scanner.nextInt()
-        println("Write how many grams of coffee beans do you want to add:")
-        beans += scanner.nextInt()
-        println("Write how many disposable cups of coffee do you want to add:")
-        cups += scanner.nextInt()
-    }
-
-    fun emptyMoney() {
+    private fun emptyMoney() {
         println("I gave you \$$money")
         money = 0
     }
+}
 
-    fun printState() {
-        println("The coffee machine has:")
-        println("$water of water")
-        println("$milk of milk")
-        println("$beans of coffee beans")
-        println("$cups of disposable cups")
-        println("$money of money")
-    }
-
-    do {
-        val action = getAction(scanner)
-        println(action)
-        when (action) {
-            "buy" -> {
-                val coffee = selectCoffee(scanner)
-                println(coffee)
-                when (coffee) {
-                    "1" -> makeCoffee(250, 0, 16, 4)
-                    "2" -> makeCoffee(350, 75, 20, 7)
-                    "3" -> makeCoffee(200, 100, 12, 6)
-                    "back" -> {
-                    }
-                }
-            }
-            "fill" -> fillMachine()
-            "take" -> emptyMoney()
-            "remaining" -> printState()
-            "exit" -> {
-            }
-        }
-    } while (action != "exit")
+enum class CoffeeMachineMenuState {
+    MAIN,
+    BUY,
+    FILL_WATER,
+    FILL_MILK,
+    FILL_BEANS,
+    FILL_CUPS,
 }
 
 fun enough(avail: Int, use: Int, res: String): Boolean {
@@ -82,15 +133,7 @@ fun enough(avail: Int, use: Int, res: String): Boolean {
     return true
 }
 
-fun selectCoffee(scanner: Scanner): String {
-    println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
-    return scanner.next()
-}
 
-fun getAction(scanner: Scanner): String {
-    println("Write action (buy, fill, take, remaining, exit): ")
-    return scanner.next()
-}
 
 
 
--- kotlin/Coffee Machine/Problems/Floor-space of the room/src/Main.kt
+++ kotlin/Coffee Machine/Problems/Floor-space of the room/src/Main.kt
@@ -10,19 +10,19 @@ fun main(args: Array<String>) {
             val a = scanner.nextDouble()
             val b = scanner.nextDouble()
             val c = scanner.nextDouble()
-            val p = (a+b+c)/2
-            val area = sqrt(p*(p-a)*(p-b)*(p-c))
+            val p = (a + b + c) / 2
+            val area = sqrt(p * (p - a) * (p - b) * (p - c))
             println(area)
         }
         "rectangle" -> {
             val a = scanner.nextDouble()
             val b = scanner.nextDouble()
 
-            println(a*b)
+            println(a * b)
         }
         "circle" -> {
             val r = scanner.nextDouble()
-            val area = 3.14*r*r
+            val area = 3.14 * r * r
             println(area)
         }
     }
